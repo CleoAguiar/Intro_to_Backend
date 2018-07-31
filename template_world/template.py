ec2231541e296313bb329addad98406c5d587d65
Cleo Aguiar <cleotaisas@gmail.com>
2018-07-31 20:36:16 -0300

    Writing a basic form

--- template_world/template.py
+++ template_world/template.py
@@ -15,13 +15,21 @@
 import os
 import webapp2
 
+form_html = """
+<form>
+<h2>Add a Food</h2>
+<input type="text" name="food">
+<button>Add</button>
+</form>
+"""
+
 class Handler(webapp2.RequestHandler):
   def write(self, *a, **kw):
     self.response.out.write(*a, **kw)
 
 class MainPage(Handler):
   def get(self):
-    self.write("Hello, Udacity!!!")
+    self.write(form_html)
 
 app = webapp2.WSGIApplication([('/', MainPage),
                               ],
