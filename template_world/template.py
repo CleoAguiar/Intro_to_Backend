ad67fa3a2b13c9a818656dda986598de664c8f57
Cleo Aguiar <cleotaisas@gmail.com>
2018-07-31 23:09:37 -0300

    Using Template Jinja2
    Something wrong, the file is not found

--- template_world/template.py
+++ template_world/template.py
@@ -13,16 +13,12 @@
 # limitations under the License.
 
 import os
+
+import jinja2
 import webapp2
 
-form_html = """
-<form>
-<h2>Add a Food</h2>
-<input type="text" name="food">
-%s
-<button>Add</button>
-</form>
-"""
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 hidden_html = """
 <input type="hidden" name="food" value="%s">
@@ -44,25 +40,35 @@ class Handler(webapp2.RequestHandler):
   def write(self, *a, **kw):
     self.response.out.write(*a, **kw)
 
+  def render_str(self, template, **params):
+   	t = jinja_env.get_template(template)
+   	return t.render(params)
+
+  def render(self, template, **kw):
+  	self.write(self.render_str(template, **kw))
+
+
 class MainPage(Handler):
   def get(self):
-  	output = form_html
+    self.render("shopping_list.html")
+
+ #  	output = form_html
   	
-  	output_hidden = ""
+ #  	output_hidden = ""
 
-  	items = self.request.get_all("food")
-  	if items:
-	  	output_items = ""
-	  	for item in items:
-	  		output_hidden += hidden_html % item
-	  		output_items += item_html % item
+ #  	items = self.request.get_all("food")
+ #  	if items:
+	#   	output_items = ""
+	#   	for item in items:
+	#   		output_hidden += hidden_html % item
+	#   		output_items += item_html % item
 
-	  	output_shopping = shopping_list_html % output_items
-	  	output += output_shopping
+	#   	output_shopping = shopping_list_html % output_items
+	#   	output += output_shopping
 
-	output = output % output_hidden
+	# output = output % output_hidden
 
-    	self.write(output)
+ #    	self.write(output)
 
 app = webapp2.WSGIApplication([('/', MainPage),
                               ],
