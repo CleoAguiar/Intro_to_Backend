7227579f6ace6e802d0b5e307d9e8e172e0aae12
Cleo Aguiar <cleotaisas@gmail.com>
2018-08-04 14:05:09 -0300

    Page rot13

---
+++ quiz/main.py
@@ -0,0 +1,51 @@
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import webapp2
+
+import os
+import jinja2
+import codecs
+
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(autoescape=True, loader = jinja2.FileSystemLoader(template_dir))
+
+class Handler(webapp2.RequestHandler):
+  def write(self, *a, **kw):
+    self.response.out.write(*a, **kw)
+
+  def render_str(self, template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+  def render(self, template, **kw):
+    self.write(self.render_str(template, **kw))
+
+
+class MainPage(Handler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Quiz ROT 13')
+
+class Rot13Handler(Handler):
+    """docstring for Rot13Handler"""
+    def get(self):
+        self.render('rot13_main.html')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/rot13', Rot13Handler),
+], debug=True)
