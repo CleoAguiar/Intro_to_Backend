bf3fd906ad96b28d2a1b719d0f2b4d3d7ba20404
Cleo Aguiar <cleotaisas@gmail.com>
2018-08-04 14:55:24 -0300

    Rot13 concluded

--- quiz/main.py
+++ quiz/main.py
@@ -16,33 +16,38 @@ import webapp2
 
 import os
 import jinja2
-import codecs
 
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(autoescape=True, loader = jinja2.FileSystemLoader(template_dir))
 
-class Handler(webapp2.RequestHandler):
-  def write(self, *a, **kw):
-    self.response.out.write(*a, **kw)
-
-  def render_str(self, template, **params):
+def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
+class BaseHandler(webapp2.RequestHandler):
   def render(self, template, **kw):
-    self.write(self.render_str(template, **kw))
+    self.response.out.write(render_str(template, **kw))
 
+  def write(self, *a, **kw):
+    self.response.out.write(*a, **kw)
 
-class MainPage(Handler):
+class MainPage(BaseHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Quiz ROT 13')
 
-class Rot13Handler(Handler):
+class Rot13Handler(BaseHandler):
     """docstring for Rot13Handler"""
     def get(self):
-        self.render('rot13_main.html')
+        self.render('rot13-main.html')
+
+    def post(self):
+        rot13 = ''
+        text = self.request.get('text')
+        if text:
+            rot13 = text.encode('rot13')
+        self.render('rot13-main.html', text = rot13)
 
 
 app = webapp2.WSGIApplication([
