faa206fe918c5586c81dc641770bdc3bad7deb70
Cleo Aguiar <cleotaisas@gmail.com>
2018-08-08 22:47:00 -0300

    Validating
    created wellcome.html, implement regular expression and error

--- quiz/main.py
+++ quiz/main.py
@@ -16,11 +16,26 @@ import webapp2
 
 import os
 import jinja2
+import re
 
+
+USER_RE = re.compile("^[a-zA-Z0-9_-]{3,20}$")
+PASSWORD_RE = re.compile("^.{3,20}$")
+EMAIL_RE = re.compile("^[\S]+@[\S]+.[\S]+$")
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(autoescape=True, loader = jinja2.FileSystemLoader(template_dir))
 
+
+def valid_username(username):
+    return USER_RE.match(username)
+
+
+def valid_password(password):
+    return PASSWORD_RE.match(password)
+
+def valid_email(email):
+    return EMAIL_RE.match(email)
 
 
 def render_str(template, **params):
@@ -39,7 +54,7 @@ class BaseHandler(webapp2.RequestHandler):
 class MainPage(BaseHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Quiz ROT 13')
+        self.response.write('Quiz ROT 13 and Signup')
 
 
 class Rot13Handler(BaseHandler):
@@ -59,10 +74,40 @@ class SignupHandler(BaseHandler):
     """docstring for SignupHandler"""
     def get(self):
         self.render('signup.html')
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        passwordVerify = self.request.get('verify')
+        email = self.request.get('email')
+
+        valid_user = valid_username(username)
+        valid_pass = valid_password(password)
+        valid_mail = valid_email(email)
+        
+        if valid_user:
+            self.redirect('/wellcome')
+        if not valid_user or not valid_pass:
+            nameError = ''
+            passError = ''
+            if not valid_user:
+                nameError = "That's not a valid username."
+            if not valid_pass:
+                passError = "That wasn't a valid password."
+            self.render('signup.html', nameError=nameError, passError=passError) 
+        else:
+            self.render('signup.html')
+
+
+class WellcomeHandler(BaseHandler):
+    """docstring for WellcomeHandler"""
+    def get(self):
+        self.render('wellcome.html', username='testeudacity')
         
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/rot13', Rot13Handler),
     ('/signup', SignupHandler),
+    ('/wellcome', WellcomeHandler),
 ], debug=True)
