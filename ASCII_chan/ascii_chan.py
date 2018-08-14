3089ec1ec6b1e49342c8bf07eb335401c9e43d23
Cleo Aguiar <cleotaisas@gmail.com>
2018-08-14 20:16:50 -0300

    Add front page

--- ASCII_chan/ascii_chan.py
+++ ASCII_chan/ascii_chan.py
@@ -24,21 +24,34 @@ jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
 class Handler(webapp2.RequestHandler):
-	"""docstring for Handler"""
-	def write(self, *a, **kw):
-		self.response.out.write(*a, **kw)
+    """docstring for Handler"""
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
 
-	def render_str(self, template, **params):
-		t = jinja_env.get_template(template)
-		return t.render(params)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
 
-	def render(self, template, **kw):
-		self.write(self.render_str(template, **kw))
-		
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+        
 
 class MainPage(Handler):
+    def render_front(self, title="", art="", error=""):
+        self.render("front.html", title=title, art=art, error=error)
+        
     def get(self):
-        self.write("ASCII chan!")
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        art = self.request.get("art")
+
+        if title and art:
+            self.write("Thanks!")
+        else:
+            error = "We need both a Title and some Artwork!"
+            self.render_front(error=error)
 
 
 app = webapp2.WSGIApplication([
