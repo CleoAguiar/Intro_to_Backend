11b6ac068067e327cf84df3b34206f43f9f46521
Cleo Aguiar <cleotaisas@gmail.com>
2018-08-15 18:34:19 -0300

    Running Queries

--- ASCII_chan/ascii_chan.py
+++ ASCII_chan/ascii_chan.py
@@ -34,11 +34,20 @@ class Handler(webapp2.RequestHandler):
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
-        
+
+
+class Art(db.Model):
+    """docstring for Art"""
+    title = db.StringProperty(required=True)
+    art = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
 
 class MainPage(Handler):
     def render_front(self, title="", art="", error=""):
-        self.render("front.html", title=title, art=art, error=error)
+        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
+
+        self.render("front.html", title=title, art=art, error=error, arts=arts)
         
     def get(self):
         self.render_front()
@@ -48,10 +57,13 @@ class MainPage(Handler):
         art = self.request.get("art")
 
         if title and art:
-            self.write("Thanks!")
+            a = Art(title=title, art=art)
+            a.put()
+
+            self.redirect("/")
         else:
             error = "We need both a Title and some Artwork!"
-            self.render_front(error=error)
+            self.render_front(title, art, error)
 
 
 app = webapp2.WSGIApplication([
