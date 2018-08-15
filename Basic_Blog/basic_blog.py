e75d4108fa7181e4ac8a56b59e71d8ab5b57ae62
Cleo Aguiar <cleotaisas@gmail.com>
2018-08-15 20:03:39 -0300

    Template newpost

--- Basic_Blog/basic_blog.py
+++ Basic_Blog/basic_blog.py
@@ -14,12 +14,48 @@
 
 import webapp2
 
+import os
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+    """docstring for Handler"""
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.write('Basic Blog!')
 
 
+class MyBlog(Handler):
+	"""docstring for MyBlog"""
+	def get(self):
+		self.render("myblog.html")
+
+
+class NewPost(Handler):
+	"""docstring for NewPost"""
+	def get(self):
+		self.render("newpost.html")
+
+		
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/blog', MyBlog),
+    ('/newpost', NewPost),
 ], debug=True)
