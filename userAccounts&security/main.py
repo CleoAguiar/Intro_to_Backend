71de307de843ec7738dfc914bf13b4a485fb8f25
Cleo Aguiar <cleotaisas@gmail.com>
2018-08-27 20:25:32 -0300

    feat: incorporating HMAC
    Improved hasg_str

--- userAccounts&security/main.py
+++ userAccounts&security/main.py
@@ -16,14 +16,18 @@ import os
 import webapp2
 import jinja2
 import hashlib
+import hmac
+
+SECRET = 'imsosecret'
 
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
+
 def hash_str(s):
-    return hashlib.md5(s).hexdigest()
+    return hmac.new(SECRET, s).hexdigest()
 
 def make_secure_val(s):
     return "%s|%s" % (s, hash_str(s))
